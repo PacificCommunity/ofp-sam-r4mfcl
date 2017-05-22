5a4f9a195dac5498426ba796e8332cfce3e88439
Yukio Takeuchi <yukiot@spc.int>
2017-05-22 11:48:00 +1100

    Updated write.ini.r for 2 sex model

--- R4MFCL.src/R4MFCL/R/write.ini.R
+++ R4MFCL.src/R4MFCL/R/write.ini.R
@@ -3,18 +3,33 @@ function(ini.file,ini.obj, old.format=FALSE) {
 ##============================================================================
 ## by Simon Hoyle June 2008
 ##  revised, PK June 2011
+##  revised, YT May 2017 for 2 sex model
 ##============================================================================
   o <- ini.obj
   #a1 <- character()
   con <- file(ini.file,open="w")
   on.exit(close(con))
-   
+
   if(with(ini.obj,exists("version")) & !old.format) {
     writeLines(c("# ini version number",o$version),con)
   }
+  if(length(o$nages)==1){
+    writeLines(c("# number of age classes",o$nages),con)
+  }else{
+    writeLines(c("# number of age classes",o$nages),con)
+    writeLines(c("# number of age classes (for female)",o$nages),con)
+  }
 
-  writeLines(c("# number of age classes",o$nages),con)
-                                                   
+  if(!is.null(o$reg.flg)){
+    writeLines("# region_flags",con)
+    write.table(o$reg.flg,con,quote=F,append=T,row.names=F,col.names=F)
+  }
+
+  if(!is.null(o$sp.flg)){
+    writeLines("#_species_flags",con)
+    write.table(o$sp.flg,con,quote=F,append=T,row.names=F,col.names=F)
+  }
+
   if(with(o,exists("tag.fish.rep"))) {
     writeLines("# tag fish rep",con)
     write.table(o$tag.fish.rep,con,quote=F,append=T,row.names=F,col.names=F)
@@ -39,23 +54,33 @@ function(ini.file,ini.obj, old.format=FALSE) {
     writeLines("# tag_fish_rep penalty",con)
     write.table(o$reppenalty,con,quote=F,append=T,row.names=F,col.names=F)
   }
-
-  writeLines(c("# maturity at age",paste(o$mat,collapse=" ")),con)
-               
-  writeLines(c("# natural mortality (per year)",o$M),con)
+  if(length(o$nages)==1){
+    writeLines(c("# maturity at age",paste(o$mat,collapse=" ")),con)
+  }else{
+    writeLines("# maturity at age of male/species1",con)
+    writeLines(paste(o$mat[1,],collapse=" "),con)
+    writeLines("# maturity at age of female/species2",con)
+    writeLines(paste(o$mat[2,],collapse=" "),con)
+  }
+  if(length(o$nages)==1){
+    writeLines(c("# natural mortality (per year)",o$M),con)
+  }else{
+    writeLines(c("# natural mortality (per year)",o$M),con)
+  }
 
   if(with(o,exists("movemap"))) writeLines(c("# movement map",paste(o$movemap,collapse=" ")),con)
-                                            
+
   if(with(o,exists("diffcoffs"))) writeLines("# diffusion coffs (per year)",con)
   write.table(o$diffcoffs,con,quote=F,append=T,row.names=F,col.names=F)
-                                             
+
 
   writeLines("# age_pars",con)
   write.table(o$age_pars,con,quote=F,append=T,row.names=F,col.names=F)
 
   writeLines(c("# recruitment distribution by region",paste(o$recbyreg,collapse=" ")),con)
 
-  writeLines(c("# The von Bertalanffy parameters",
+  if(length(o$nages)==1){
+    writeLines(c("# The von Bertalanffy parameters",
                "# Initial  lower bound  upper bound",
                "# ML1",
                 paste(paste(o$VBLmin,collapse=" ")),
@@ -63,14 +88,50 @@ function(ini.file,ini.obj, old.format=FALSE) {
                 paste(paste(o$VBLmax,collapse=" ")),
                "# K (per year)",
                 paste(paste(o$VBK,collapse=" ")),
-               "# Length-weight parameters",            
+               "# Length-weight parameters",
                 paste(paste(o$LW,collapse=" ")),
                "# sv(29)",
-                o$sv29,
+                o$steepness,
                "# Generic SD of length at age",
                 paste(paste(o$sdLatA,collapse=" ")),
                "# Length-dependent SD",
                 paste(paste(o$Ldep_sd,collapse=" ")),
                "# The number of mean constraints",
                 paste(paste(o$Nmeanconstr,collapse=" "))), con)
+  }else{
+    writeLines(c("# The von Bertalanffy parameters of male/species1",
+               "# Initial  lower bound  upper bound",
+               "# ML1",
+                paste(paste(o$VBLmin[1,],collapse=" ")),
+               "# ML2",
+                paste(paste(o$VBLmax[1,],collapse=" ")),
+               "# K (per year)",
+                paste(paste(o$VBK[1,],collapse=" ")),
+               "# Length-weight parameters",
+                paste(paste(o$LW[1,],collapse=" ")),
+               "# sv(29), steepness",
+                o$steepness[1],
+               "# The von Bertalanffy parameters of female/species2",
+               "# Initial  lower bound  upper bound",
+               "# ML1",
+                paste(paste(o$VBLmin[2,],collapse=" ")),
+               "# ML2",
+                paste(paste(o$VBLmax[2,],collapse=" ")),
+               "# K (per year)",
+                paste(paste(o$VBK[2,],collapse=" ")),
+               "# Length-weight parameters",
+                paste(paste(o$LW[2,],collapse=" ")),
+               "# sv(29), steepness",
+                o$steepness[2],
+               "# Generic SD of length at age",
+                paste(paste(o$sdLatA[1,],collapse=" ")),
+               "# Length-dependent SD",
+                paste(paste(o$Ldep_sd[1,],collapse=" ")),
+                "# Generic SD of length at age",
+                paste(paste(o$sdLatA[2,],collapse=" ")),
+               "# Length-dependent SD",
+                paste(paste(o$Ldep_sd[2,],collapse=" ")),
+               "# The number of mean constraints",
+                paste(paste(o$Nmeanconstr,collapse=" "))), con)
+  }
 }
