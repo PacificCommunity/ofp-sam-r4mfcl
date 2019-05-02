041fb3c4040693beeeab2e699687f8c277b31587
Nicholas Ducharme-Barth <nicholasd@spc.int>
2019-05-02 19:41:42 +1100

    Update convert_mult_frq_vsn9.r
    commented out the script since it was breaking the build of R4MFCL

--- R4MFCL.src/R4MFCL/R/convert_mult_frq_vsn9.r
+++ R4MFCL.src/R4MFCL/R/convert_mult_frq_vsn9.r
@@ -1,40 +1,43 @@
-# Convert multi-spp/sex .frq file from version 8 to version 9
-source("C:/Nick/MFCL/Testing/R_toolbox/read.mult.frq.R")
-source("C:/Nick/MFCL/Testing/R_toolbox/write.mult.frq.R")
+# this is a script that Nick Davies uses to convert from .frq vsn 8 to vsn 9 
+# commented out by Nicholas Ducharme-Barth 02/05/2019 since it breaks the build of R4MFCL with its hardwired source calls
 
-#wkdir <- c("C:/Nick/Linux64VM_sync/VM64_compltn/Testing/MTHRD/devvsn7_mult_spp/")
+# # Convert multi-spp/sex .frq file from version 8 to version 9
+# source("C:/Nick/MFCL/Testing/R_toolbox/read.mult.frq.R")
+# source("C:/Nick/MFCL/Testing/R_toolbox/write.mult.frq.R")
 
-wkdir <- c("C:/Nick/MFCL/sppsex/Testing/2017-08-11_sngl_eval_mult_sex/devvsn7_mult_sex/")
+# #wkdir <- c("C:/Nick/Linux64VM_sync/VM64_compltn/Testing/MTHRD/devvsn7_mult_spp/")
 
-setwd(wkdir)
-#
-#fln <- c("mult5.frq")
-fln <- c("mult_eqsex.frq")
-dat <- read.mult.frq(fln)
-#
-# Change data structure flag for version of .frq to 9
-dat$struct$te <- 9
-#
-# Insert replicate columns for data aggregation flags sppagg_1 sppagg_2
-# for LF and WF data
-mat <- dat$mat
-head(mat)
-tmp <- mat[,c("sppagg_1","sppagg_2")]
-tmplf <- tmp
-dimnames(tmplf)[[2]] <- c("sppagglf_1","sppagglf_2")
-tmpwf <- tmp
-dimnames(tmpwf)[[2]] <- c("sppaggwf_1","sppaggwf_2")
-mat2 <- cbind(mat[,c(1:8)],tmplf,tmpwf,mat[,c(9:dim(mat)[2])])
-head(mat2)
-# re-number fisheries data columns
-dimnames(mat2)[[2]][c(16:dim(mat2)[2])] <- as.character(c(16:dim(mat2)[2]))
-#
-# Restore into .frq object
-dat$mat <- mat2
-#
-# Write to file
-frqfile <- c("mult_eqsex_vsn9.frq")
-frq.obj <- dat
-write.mult.frq(frqfile,frq.obj)
-#
-################################################################################
+# wkdir <- c("C:/Nick/MFCL/sppsex/Testing/2017-08-11_sngl_eval_mult_sex/devvsn7_mult_sex/")
+
+# setwd(wkdir)
+# #
+# #fln <- c("mult5.frq")
+# fln <- c("mult_eqsex.frq")
+# dat <- read.mult.frq(fln)
+# #
+# # Change data structure flag for version of .frq to 9
+# dat$struct$te <- 9
+# #
+# # Insert replicate columns for data aggregation flags sppagg_1 sppagg_2
+# # for LF and WF data
+# mat <- dat$mat
+# head(mat)
+# tmp <- mat[,c("sppagg_1","sppagg_2")]
+# tmplf <- tmp
+# dimnames(tmplf)[[2]] <- c("sppagglf_1","sppagglf_2")
+# tmpwf <- tmp
+# dimnames(tmpwf)[[2]] <- c("sppaggwf_1","sppaggwf_2")
+# mat2 <- cbind(mat[,c(1:8)],tmplf,tmpwf,mat[,c(9:dim(mat)[2])])
+# head(mat2)
+# # re-number fisheries data columns
+# dimnames(mat2)[[2]][c(16:dim(mat2)[2])] <- as.character(c(16:dim(mat2)[2]))
+# #
+# # Restore into .frq object
+# dat$mat <- mat2
+# #
+# # Write to file
+# frqfile <- c("mult_eqsex_vsn9.frq")
+# frq.obj <- dat
+# write.mult.frq(frqfile,frq.obj)
+# #
+# ################################################################################
